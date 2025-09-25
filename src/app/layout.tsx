d079760b31ffff9af7a89260bb91711074ab48f2
naywvi <nagib.lakhdari.pro@gmail.com>
2025-09-25 19:00:47 +0200

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "~/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Nagib Lakhdari - Portfolio",
-  description: "A Portfolio template built with Next.js",
+  description: "Passionné par le développement, je conçois des applications performantes et accessibles. Je m’intéresse à la sécurité et aux bonnes pratiques pour créer des solutions fiables. Curieux, j’explore sans cesse de nouvelles approches. Et j’aime les oiseaux.",
   icons: [{ url: "/swirl.svg", rel: "icon" }],
 };
 
